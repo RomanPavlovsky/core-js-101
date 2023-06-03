41c62069d3f6c6a52040cbb7930285c2b9f47c62
Roman.P <killme15139214@gmail.com>
2023-06-03 22:37:58 +0300

    feat: complete promisses tasks  (Sat, Jun 03, 2023 10:37:58 PM)

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -28,10 +28,20 @@
  *      .catch((error) => console.log(error.message)) // 'Error: Wrong parameter is passed!
  *                                                    //  Ask her again.';
  */
-function willYouMarryMe(/* isPositiveAnswer */) {
-  throw new Error('Not implemented');
+function willYouMarryMe(isPositiveAnswer) {
+  return new Promise((resolve, reject) => {
+    if (typeof isPositiveAnswer === 'boolean') {
+      if (isPositiveAnswer === true) {
+        resolve('Hooray!!! She said "Yes"!');
+      }
+      if (isPositiveAnswer === false) {
+        resolve('Oh no, she said "No".');
+      }
+    } else {
+      reject(new Error('Wrong parameter is passed! Ask her again.'));
+    }
+  });
 }
-
 
 /**
  * Return Promise object that should be resolved with array containing plain values.
@@ -48,8 +58,8 @@ function willYouMarryMe(/* isPositiveAnswer */) {
  *    })
  *
  */
-function processAllPromises(/* array */) {
-  throw new Error('Not implemented');
+function processAllPromises(array) {
+  return Promise.all(array);
 }
 
 /**
@@ -71,9 +81,10 @@ function processAllPromises(/* array */) {
  *    })
  *
  */
-function getFastestPromise(/* array */) {
-  throw new Error('Not implemented');
+function getFastestPromise(array) {
+  return Promise.race(array);
 }
+
 
 /**
  * Return Promise object that should be resolved with value that is
@@ -92,8 +103,22 @@ function getFastestPromise(/* array */) {
  *    });
  *
  */
-function chainPromises(/* array, action */) {
-  throw new Error('Not implemented');
+async function chainPromises(array, action) {
+  let result;
+  try {
+    const getResolve = () => {
+      const arr = [];
+      for (let i = 0; i < array.length; i += 1) {
+        array[i].then((res) => arr.push(res));
+      }
+      return arr;
+    };
+    const newArr = await getResolve();
+    result = newArr.reduce(action);
+  } catch (error) {
+    return new Error('ERROR');
+  }
+  return result;
 }
 
 module.exports = {
